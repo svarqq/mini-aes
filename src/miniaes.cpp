694000925da6077d0063b50f92e447200fc1e88c
Sean Gloumeau <svarq@dhcp-171-157.monte.halls.soton.ac.uk>
2021-12-13 13:23:50 +0000

    Added functionality for encrypting and decrypting files or text from stdin

--- src/miniaes.cpp
+++ src/miniaes.cpp
@@ -1,8 +1,12 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <cstring>
+#include <cstring>
 
 #include "aux.h"
 #include "key.h"
@@ -10,7 +14,10 @@
 #include "nibblesub.h"
 #include "shiftrow.h"
 
-std::string encrypt(std::string plaintext, unsigned short secret_key, bool verbose) {
+
+unsigned short SECRET_KEY = 2666;
+
+std::string encrypt(std::string plaintext, unsigned short secret_key) {
     const int num_blks = (int)round((float)plaintext.length()/2);
     unsigned short plaintext_blks[num_blks];
     unsigned short keys[NUMKEYS];
@@ -41,7 +48,7 @@ std::string encrypt(std::string plaintext, unsigned short secret_key, bool verbo
     return blks_to_text(plaintext_blks, num_blks);
 }
 
-std::string decrypt(std::string enctext, unsigned short secret_key, bool verbose) {
+std::string decrypt(std::string enctext, unsigned short secret_key) {
     const int num_blks = (int)round((float)enctext.length()/2);
     unsigned short enctext_blks[num_blks];
     unsigned short keys[NUMKEYS];
@@ -75,15 +82,63 @@ std::string decrypt(std::string enctext, unsigned short secret_key, bool verbose
 
 
 
-int main() {
-    std::string plaintext = "Hello world!!";
-    srand(time(NULL));
-    unsigned short secret_key = rand() % (int)pow(2, NUMBITS_BLK);
+int main(int argc, char **argv) {
+    // check for legal arguments
+    std::string aes_process;
+    std::string aes_target;
+    if (argc == 3) {
+        if (strcmp(argv[1], "encrypt") == 0 || strcmp(argv[1], "decrypt") == 0) {
+            aes_process = argv[1];
+            aes_target = argv[2];
+        } else {
+            throw std::invalid_argument("First arguments must be either 'encrypt' or 'decrypt'\n"
+                    "\t\tmini_aes [encrypt|decrypt] [text|filepath]");
+        }
+    } else {
+        throw std::invalid_argument("Two arguments must be provided to mini_aes\n"
+                "\t\tmini_aes [encrypt|decrypt] [text|filepath]");
+    }
 
-    std::string tmp = encrypt(plaintext, secret_key, false);
+    // load text either as file input or argument input
+    std::string intext;
+    std::ifstream infile(aes_target);
+    bool file_input = false;
+    if (infile.is_open()) {
+        file_input = true;
+        intext = get_filetext(infile);
+        infile.close();
+    } else {
+        intext = aes_target;
+    }
 
-    std::string tmp2 = decrypt(tmp, secret_key, false);
-    
+    // encrypt or decrypt text depending on provided argument
+    std::string outtext;
+    if (aes_process.compare("encrypt") == 0) {
+        outtext = encrypt(intext, SECRET_KEY);
+    } else if (aes_process.compare("decrypt") == 0) {
+        outtext = decrypt(intext, SECRET_KEY);
+    }
+
+
+    if (file_input) {
+        std::string out_fname;
+        int directory_idx = aes_target.rfind("/");
+        int extension_idx = aes_target.rfind(".");
+        std::cout << directory_idx << " | " << extension_idx << "\n";
+        if (aes_process.compare("encrypt") == 0) {
+            out_fname += "encrypted/";
+            out_fname += aes_target.substr(directory_idx + 1, extension_idx-directory_idx-1);
+            out_fname += ".enc";
+        } else if (aes_process.compare("decrypt") == 0) {
+            out_fname += "decrypted/";
+            out_fname += aes_target.substr(directory_idx + 1, extension_idx-directory_idx-1);
+            out_fname += ".dec";
+        }
+        write_file(out_fname, outtext);
+    } else {
+        print_formatted_result(intext, outtext);
+    }
 
     return 0;
 }
+
